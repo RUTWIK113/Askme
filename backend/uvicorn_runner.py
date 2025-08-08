df4069fa6e53221d2454763096f82fd6c477743e
RUTWIK113 <rutwiknakkalla03062005@gmail.com>
2025-08-08 19:35:25 +0530

    host change

--- backend/uvicorn_runner.py
+++ backend/uvicorn_runner.py
@@ -6,7 +6,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         reload=True, # The server will restart on code changes
         log_level="info"
